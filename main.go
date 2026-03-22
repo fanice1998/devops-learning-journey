9fe3b43745c6e1db60437b85702f716406a90834
fanice1998 <fanice0721@gmail.com>
2026-03-22 17:38:55 +0800

    Feat: 新增日期顯示與成功訊息

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 )
 
 func main() {
-	name := flag.String("name", "Devops learner", "Enter your name")
+	name := flag.String("name", "Devops 學習", "輸入你的名字")
 	flag.Parse()
-	fmt.Printf("Hello %s!, This my DevOps learning journey~\n", *name)
+	fmt.Printf("Hello %s!, 這是我的 DevOps 學習旅程~\n", *name)
+	fmt.Printf("今天是 %s，我成功用 ENTRYPOINT 跑起來了", time.Now().Format("2006-01-02"))
 }
