7117ce36c03f270b9d82250eaebd31eaaaef767c
fanice1998 <fanice0721@gmail.com>
2026-03-22 18:30:41 +0800

    Chore: Modify the words used in the output

--- main.go
+++ main.go
@@ -10,15 +10,14 @@ import (
 )
 
 func main() {
-	name := flag.String("name", "Devops 學習", "輸入你的名字")
+	name := flag.String("name", "DevOps 學習者", "輸入你的名字")
 	owner := flag.String("owner", "yourusername", "GitHub 使用者名稱")
 	repo := flag.String("repo", "devops-learning-journey", "repo 名稱")
 	flag.Parse()
 
-	fmt.Printf("Hello %s!, 這是我的 DevOps 學習旅程~\n", *name)
-	fmt.Printf("今天是 %s，我成功用 ENTRYPOINT 跑起來了\n", time.Now().Format("2006-01-02"))
+	fmt.Printf("Hello %s\n", *name)
+	fmt.Printf("今天日期：%s\n", time.Now().Format("2006-01-02"))
 
-	// Call GitHub API get commit count
 	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/commits", *owner, *repo)
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("User-Agent", "my-devops-tool")
@@ -31,12 +30,14 @@ func main() {
 		var commits []interface{}
 		err = json.Unmarshal(body, &commits)
 		if err != nil {
-			fmt.Printf("JSON parase error, err: %v\n", err)
+			fmt.Printf("JSON 解析錯誤: %v\n", err)
 			return
 		}
-		fmt.Printf("你的 repo 目前有 %d 個 commit!\n", len(commits))
+		fmt.Printf("目前 commit 數量：%d\n", len(commits))
 	} else {
-		fmt.Println("(無法連上 GitHub... )")
-		fmt.Printf("GitHub repsponse, err: %v", err)
+		fmt.Println("無法連上 GitHub")
+		if err != nil {
+			fmt.Printf("錯誤: %v\n", err)
+		}
 	}
 }
